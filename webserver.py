2d228ac0c91c0dd09843c6e34939e24561cc7a54
AlifHN <122423594+AlifHN@users.noreply.github.com>
2023-05-07 08:17:10 +0700

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -35,13 +35,13 @@ def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('localhost', 8080))
     server_socket.listen()
-    print("Server listening on localhost:8080")
+    print("Server berjalan di localhost:8080")
 
     while True:
         client_socket, client_address = server_socket.accept()
-        print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+        print(f"Menerima koneksi dari {client_address[0]}:{client_address[1]}")
         request = client_socket.recv(1024).decode()
-        print(f"Received request: {request.split()[1]}")
+        print(f"Mendapatkan request: {request.split()[1]}")
         handle_request(client_socket, request)
 
 if __name__ == "__main__":
